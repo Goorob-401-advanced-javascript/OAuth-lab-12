b32ac471bcb2517a360ef4507765ab8c5c7b820b
LTUC Student 1 <std1@QUDSLABS.LOCAL>
2020-02-04 07:35:20 +0200

    working to figure out how to work on oauth

--- oauth-middleware.js
+++ oauth-middleware.js
@@ -7,7 +7,7 @@ const tokenServerUrl = 'https://github.com/login/oauth/access_token';
 const remoteAPI = 'https://api.github.com/user';
 const CLIENT_ID = process.env.CLIENT_ID ;
 const CLIENT_SECRET = process.env.CLIENT_SECRET ;
-const API_SERVER = 'http://localhost:3000/oauth';
+const API_SERVER = process.env.API_SERVER ;
 
 module.exports = async function authorize(req, res, next) {
   try {
